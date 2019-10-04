e17e8232f0773aaaf8072fd6deca9a5b5ac229fd
nicolasprantl <nicolasmartinsextocsj@gmail.com>
2019-10-04 19:22:31 -0300

    Agrego a los orcos

---
+++ src/Library/characters/Orco.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using RoleplayGame.Items;
+
+namespace RoleplayGame.Characters
+{
+    /// <summary>
+    /// Personaje Orco
+    /// </summary>
+    public class Orco : Character
+    {
+        public Orco(string name)
+            : base(name)
+        {
+            this.Health = 200;
+            this.AddItem(new Coraza());
+            this.AddItem(new Palo());
+        }
+    }
