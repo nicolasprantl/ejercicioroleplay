2a20f6ea895faf23c7f4b08aa81631568713867a
Facundo-Martiarena <53951865+Facundo-Martiarena@users.noreply.github.com>
2019-10-04 19:00:17 -0300

    agrego Termo

---
+++ src/Library/items/Termo.cs
@@ -0,0 +1,30 @@
+namespace RoleplayGame.Items
+{
+    /// <summary>
+    /// Magia. Permite atacar y defender.
+    /// </summary>
+    public class Termo : IAttackItem
+    {
+        /// <summary>
+        /// El poder de ataque
+        /// </summary>
+        /// <value></value>
+        public int AttackPower
+        {
+            get
+            {
+                return 30;
+            }
+        }
+
+        /// <summary>
+        /// El poder de defensa
+        /// </summary>
+        /// <value></value>
+        
+        public override string ToString()
+        {
+            return "Termo";
+        }
+    }
+}
